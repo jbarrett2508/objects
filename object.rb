92b0e76ddad9fbf22113e916d1d64af7f6e90751
Judith Barrett <judith.barrett@gmail.com>
2014-10-15 14:14:46 -0600

    Modified object.rb

--- object.rb
+++ object.rb
@@ -4,18 +4,28 @@ class Run
     @time = time #number of hours run
   end
   def pace
-    @miles / @time
-  end
-  def speed_up
-    pace > 8.0
-  end
-  def slow_down
-    pace < 7.0
-  end
-  def finish
-    pace = 0
+    @pace = @miles / @time
+    puts "Your pace is #{@pace} miles per hour."
+
+    if @pace <= 6.0
+    puts "Speed up!"
+    end
+
+    if @pace >= 8.0
+    puts "Slow down!"
+    end
+
+    if @pace == 0
+    puts "Nice job! You're done!"
+    end
   end
 end
 
-run = Run.new(6.0, 1.25)
-puts "Pace is #{run.pace} miles per hour"
+run1 = Run.new(6.0, 1.25)
+puts run1.pace
+run2 = Run.new(5.0, 1.25)
+puts run2.pace
+run3 = Run.new(7.5, 1.00)
+puts run3.pace
+run4 = Run.new(8.2, 1.00)
+puts run4.pace
